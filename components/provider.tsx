7f5493518462f0419dc09224230aa51b44002a16
Tomas Padilla <padillatomasagustin@gmail.com>
2025-04-04 17:43:32 +0200

    [MUL-2] add provider

---
+++ components/provider.tsx
@@ -0,0 +1,6 @@
+// components/provider.tsx
+import "mokku-ui-library/dist/styles.css";
+
+export const MokkuProvider = ({ children }: { children: React.ReactNode }) => {
+	return <>{children}</>;
+};
