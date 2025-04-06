fd151378200456a5582135f3b35395bda10e437d
Tomas Padilla <padillatomasagustin@gmail.com>
2025-04-06 20:57:53 +0200

    [MUL-2] PROJECT CODE STYLE

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,35 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { FlatCompat } from '@eslint/eslintrc'
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+    baseDirectory: __dirname,
+    recommendedConfig: {
+        env: {
+            browser: true,
+            es2021: true
+        },
+        parserOptions: {
+            ecmaVersion: 'latest',
+            sourceType: 'module'
+        }
+    }
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+    {
+        ignores: ['dist/**/*', 'node_modules/**/*', 'storybook-static/**/*']
+    },
+    ...compat.extends('next/core-web-vitals', 'next', 'plugin:@typescript-eslint/recommended', 'prettier'),
+    {
+        rules: {
+            // 🔕 Desactivar esta regla que espera /pages o /app
+            '@next/next/no-html-link-for-pages': 'off'
+        }
+    }
+]
 
-export default eslintConfig;
+export default eslintConfig
